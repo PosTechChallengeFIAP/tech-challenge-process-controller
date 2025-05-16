f7ce4434540b53a5780d95109289d4d79df367e9
First Last <you@email.com>
2025-05-15 22:56:49 -0300

    aJUSTE NO patch

--- src/main/java/com/tech/challenge/QueueService/adapters/controllers/QueueController.java
+++ src/main/java/com/tech/challenge/QueueService/adapters/controllers/QueueController.java
@@ -75,10 +75,10 @@ public class QueueController {
             @ApiResponse(description = "Unauthorized Access", responseCode = "401", content = @Content),
             @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
     })
-    public ResponseEntity updateQueue(@PathVariable Integer queueId, @RequestBody Queue queue){
+    public ResponseEntity updateQueue(@PathVariable Integer queueItemId, @RequestBody Queue queue){
 
         try{
-            UpdateQueueUseCaseRequest queueRequest = new UpdateQueueUseCaseRequest(queueId,queue.getStatus());
+            UpdateQueueUseCaseRequest queueRequest = new UpdateQueueUseCaseRequest(queueItemId,queue.getStatus());
             Queue updatedQueue = updateQueueUseCase.execute(queueRequest);
             return ResponseEntity.status(HttpStatus.OK).body(updatedQueue);
 
