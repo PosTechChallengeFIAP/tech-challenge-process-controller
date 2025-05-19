b7d85eafa96f5e117e44620f33b451bb6eb4af2e
First Last <you@email.com>
2025-05-18 21:08:56 -0300

    UnitTest para CreateQueue ajustado

--- src/test/java/com/tech/challenge/QueueService/core/domain/useCases/CreateQueueUseCaseTest.java
+++ src/test/java/com/tech/challenge/QueueService/core/domain/useCases/CreateQueueUseCaseTest.java
@@ -1,8 +1,10 @@
 package com.tech.challenge.QueueService.core.domain.useCases;
 
+import com.tech.challenge.QueueService.adapters.infra.impl.QueueRepository;
 import com.tech.challenge.QueueService.core.domain.entities.Queue;
 import com.tech.challenge.QueueService.core.domain.repositories.IQueueRepository;
 import com.tech.challenge.QueueService.core.domain.useCases.CreateQueueUseCase.CreateQueueUseCase;
+import com.tech.challenge.QueueService.core.domain.useCases.CreateQueueUseCase.ICreateQueueUseCase;
 import com.tech.challenge.QueueService.core.domain.useCases.FindNotFinishedItemsUseCase.FindNotFinishedItemsUseCase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -21,16 +23,14 @@ public class CreateQueueUseCaseTest {
     private CreateQueueUseCase createQueueUseCase;
 
     @MockBean
-    private IQueueRepository queueRepository;
+    private QueueRepository queueRepository;
 
-    @MockBean
-    private FindNotFinishedItemsUseCase findNotFinishedItemsUseCase;
     @Test
     public void executeTest() throws Exception{
 
-        Queue queue = mock(Queue.class);
+        Queue queue = new Queue();
 
-        when(createQueueUseCase.execute(queue)).thenReturn(queue);
+        when(queueRepository.save(queue)).thenReturn(queue);
 
         Queue insertedQueue = createQueueUseCase.execute(queue);
 
